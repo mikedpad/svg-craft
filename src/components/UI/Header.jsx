00f6be26d8f218acdee12acf78f08cfff9cf5b35
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2020-03-09 10:53:35 -0700

    Relocated menu / nav components

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/styles';
-import SideMenu from '../Menu/SideMenu';
+import SideMenu from './Menu/SideMenu';
 import Title from './Title';
 
 const useStyles = makeStyles({
