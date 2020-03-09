5e362443b2227e09a52f9c94bc2b61a83a2a22ea
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2020-03-09 10:54:12 -0700

    Removed styled components

--- src/components/SVGCanvas.jsx
+++ src/components/SVGCanvas.jsx
@@ -1,71 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Grid from './Grid';
-
-const SVG = styled.svg`
-  background-color: orange;
-  height: auto;
-  width: 100%;
-`;
+import Circle from './Shapes/Circle';
 
 const SVGCanvas = ({ dimensions }) => {
   const [width, height] = dimensions;
   const origin = [-width / 2, -height / 2];
 
   return (
-    <SVG
+    <svg
       viewBox={`${origin[0]} ${origin[1]} ${width} ${height}`}
       xmlns="http://www.w3.org/2000/svg"
       xmlns-xlink="http://www.w3.org/1999/xlink"
+      style={{ height: `auto`, width: `100%` }}
     >
-      {/* definitions */}
-      <Grid origin={origin} dimensions={dimensions} />
-      <defs>
-        {/* Shapes */}
-
-        <g id="sphere">
-          <radialGradient id="rg" cx="33%" cy="33%">
-            <stop offset="0%" stopColor="#f01" />
-            <stop offset="40%" stopColor="#d01" />
-            <stop offset="100%" stopColor="#600" />
-          </radialGradient>
-          <circle r="5" fill="url(#rg)" />
-        </g>
-
-        <g id="rod">
-          <linearGradient id="lg" x1="50%" y1="20%" x2="90%" y2="80%">
-            <stop offset="0%" stopColor="#901" />
-            <stop offset="100%" stopColor="#300" />
-          </linearGradient>
-          <rect x="-2.5" width="5" height="20" rx="2.3" ry="2.3" fill="url(#lg)" />
-        </g>
-
-        {/* <circle id='c' r='5' stroke='#000' stroke-width='1' fill='#f0f' /> */}
-        <path
-          id="arc"
-          d="M-5 0 c 0 -6.75 10 -6.75 10 0"
-          fill="none"
-          vectorEffect="non-scaling-stroke"
-          strokeWidth="25"
-        />
-      </defs>
-
-      {/* objects */}
-
-      <path className="blackLine" d="M 150 250 c 0 -135 200 -135 200 0" />
-
-      <use xlinkHref="#rod" />
-      <use xlinkHref="#sphere" />
-      <use xlinkHref="#arc" className="arc1" />
-      <use xlinkHref="#arc" className="arc2" transform="scale(0.5)" />
-
-      {/* debug bezier circle points */}
-      {/*
-    <use xlink:href='#c' x='150' y='115' />
-    <use xlink:href='#c' x='350' y='115' />
-    */}
-    </SVG>
+      <Grid id="grid" origin={origin} dimensions={dimensions} />
+      {/* <Circle id="circle1" radius={10} x={-100} y={-60} fill="#f00" /> */}
+      <Circle
+        id="circle1"
+        radius={40}
+        x={10}
+        y={10}
+        fill={{
+          id: `rgb`,
+          cx: 10,
+          cy: 10,
+          stops: [
+            {
+              offset: 0,
+              stopColor: `#f01`,
+            },
+            {
+              offset: 30,
+              stopColor: `#c00`,
+            },
+            {
+              offset: 45,
+              stopColor: `#0c0`,
+            },
+            {
+              offset: 100,
+              stopColor: `#050`,
+            },
+          ],
+        }}
+      />
+    </svg>
   );
 };
 
